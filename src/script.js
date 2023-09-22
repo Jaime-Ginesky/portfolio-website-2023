05d54277b80908b57c5f28a44d997262614ed0f0
Jaime-Ginesky <jaimelee79@gmail.com>
2023-09-21 23:56:36 -0700

    added slide-up on h3

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const theme = document.getElementById("theme");
 let darkMode = localStorage.getItem("dark-mode");
 
 
-
+// Function to create Dark/Light Mode button
 const enableDarkMode = () => {
     theme.classList.add("dark-mode-theme");
     toggleBtn.classList.remove("dark-mode-toggle");
@@ -29,4 +29,56 @@ toggleBtn.addEventListener("click", (e) => {
         disableDarkMode();
     }
 });
+
+  // Function to create a card
+  function createCard(title, description, link) {
+    const card = document.createElement('div');
+    card.classList.add('imgCard');
+    // Add your desired card class here
+
+    const titleElement = document.createElement('h4');
+    titleElement.textContent = title;
+
+    const contentElement = document.createElement('div');
+    contentElement.classList.add('content');
+
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = description;
+
+    const linkElement = document.createElement('a');
+    linkElement.href = link;
+    linkElement.classList.add('button', 'glow-button');
+    linkElement.target = '_blank';
+    linkElement.textContent = 'GO';
+
+    contentElement.appendChild(descriptionElement);
+    contentElement.appendChild(linkElement);
+
+    card.appendChild(titleElement);
+    card.appendChild(contentElement);
+
+    return card;
+  }
+
+  // Populate the cards with specific information
+  const weatherCard = createCard(
+    'Weather App',
+    'Find current weather in your current location or search any city. Search also includes weather conditions and forecast. Built with Javascript.',
+    'https://goofy-goldstine-7355dd.netlify.app/'
+  );
+  document.getElementById('weather-card').appendChild(weatherCard);
+
+  const robofriendsCard = createCard(
+    'RoboFriends',
+    'For each name entered into search, a new RoboFriend avatar is created and attached to that person. Built with React.',
+    'https://upbeat-bohr-d8d654.netlify.app/'
+  );
+  document.getElementById('robofriends-card').appendChild(robofriendsCard);
+
+  const dictionaryCard = createCard(
+    'Dictionary App',
+    'Find definitions, synonyms, pronunciation, with sentence examples, audio and images. Built with React.',
+    'https://magical-sunshine-e3e2c8.netlify.app/'
+  );
+  document.getElementById('dictionary-card').appendChild(dictionaryCard);
 
