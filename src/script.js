f4fc9e63172893013ee8df69b249167c1e55fae1
Jaime-Ginesky <jaimelee79@gmail.com>
2024-02-15 14:59:28 -0800

    performance

--- src/script.js
+++ src/script.js
@@ -1,15 +1,16 @@
 'use strict'
 
-window.onload = () => {
-    setTimeout(function() {
-      document.getElementsByTagName("body");
-    }, 200);
-  }
+
 
 const toggleBtn = document.getElementById("toggle-btn");
 const theme = document.getElementById("theme");
 let darkMode = localStorage.getItem("dark-mode");
 
+window.onload = () => {
+    setTimeout(function() {
+      document.getElementsByTagName("body");
+    }, 200);
+  }
 
 
 
