03df13554b472fd66240f3fd1caf1ae3bd2aea3c
Jaime-Ginesky <jaimelee79@gmail.com>
2024-02-16 17:53:16 -0800

    removed unnecessary code

--- src/script.js
+++ src/script.js
@@ -5,30 +5,12 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const theme = document.getElementById("theme");
 let darkMode = localStorage.getItem("dark-mode");
-// const navLink = document.querySelector(".nav-link-active");
-// const navItem = document.querySelector("nav-item");
-
-// navLink.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     navLink.classList.add("nav-link-active","nav-link-active:hover",
-//     "nav-list", "nav-item", "button", "button:hover")
-// })
-
-
-
-//     let output ='<ul>'
-//     for(let item of arr) {
-//         output += `<li><a class="${item.class}" href="${item.href}">${item.text}</a>
-//                     <button class="${item.class}" text="${item.text}"</button><li>`
-//      }
-// }
 
 window.onload = () => {
     setTimeout(function() {
       document.getElementsByTagName("body");
     }, 200);
   }
-
 
 
 // Function to create Dark/Light Mode button
